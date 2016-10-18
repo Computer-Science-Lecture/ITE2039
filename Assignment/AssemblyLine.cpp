6aade28009b987cf0eb6caf7a3b6319cb7f6d2e2
MaybeS <maytryark@gmail.com>
2016-10-18 14:47:42 +0900

    Finish AssemblyLine

--- Assignment/AssemblyLine.cpp
+++ Assignment/AssemblyLine.cpp
@@ -1,95 +1,91 @@
-//2015004584_Baejiun_A
+// 2015004584_배지운_A
 #include <stdio.h>
 #include <stdlib.h>
-#include <vector>
 #include <algorithm>
-#include <stack>
 
 using namespace std;
 
-int main(int argc, char * argv[])
+int main(int argc, char * argv[]) 
 {
-	int temp;
-	int N;
-	pair<int, int> e, x;
-	pair< vector<int>, vector<int> > a, t;
-	pair< vector<bool>, vector<bool> > r;
+    int count, e1, e2, ex1, ex2;
+    int *a1, *a2, *t1, *t2, *r1, *r2, *route, *result;
 
-	// input
-	scanf("%d", &N);
-	scanf("%d %d %d %d", &e.first, &e.second, &x.first, &x.second);
+    scanf("%d", &count);
 
-	for (int i = 0; i < N; ++i)
-	{
-		scanf("%d", &temp);
-		a.first.push_back(temp);
-	}
-	for (int i = 0; i < N; ++i)
-	{
-		scanf("%d", &temp);
-		a.second.push_back(temp);
-	}
-	for (int i = 0; i < N - 1; ++i)
-	{
-		scanf("%d", &temp);
-		t.first.push_back(temp);
-	}
-	for (int i = 0; i < N - 1; ++i)
-	{
-		scanf("%d", &temp);
-		t.second.push_back(temp);
-	}
+    a1 = (int*)malloc(count * sizeof(int));
+    a2 = (int*)malloc(count * sizeof(int));
+    t1 = (int*)malloc(count * sizeof(int));
+    t2 = (int*)malloc(count * sizeof(int));
+    r1 = (int*)malloc(count * sizeof(int));
+    r2 = (int*)malloc(count * sizeof(int));
+    result = (int*)malloc(count * sizeof(int));
 
-	// init
-	a.first[0] += e.first;
-	a.second[0] += e.second;
+    scanf("%d %d", &e1, &e2);
+    scanf("%d %d", &ex1, &ex2);
 
-	// process
-	for (int i = 1; i < N; ++i)
-	{
-		temp = min(a.second[i - 1] + t.second[i - 1], a.first[i - 1]);
-		a.first[i] += temp;
-		r.first.push_back(temp == a.second[i - 1] + t.second[i - 1]);
 
-		temp = min(a.first[i - 1] + t.first[i - 1], a.second[i - 1]);
-		a.second[i] += temp;
-		r.second.push_back(temp == a.second[i - 1]);
-	}
+    for (int i = 0; i < count; ++i) {
+        scanf("%d", a1+i);
+    }
+    for (int i = 0; i < count; ++i) {
+        scanf("%d", a2+i);
+    }
+    for (int i = 0; i < count-1; ++i) {
+        scanf("%d", t1+i);
+    }
+    for (int i = 0; i < count-1; ++i) {
+        scanf("%d", t2+i);
+    }
 
-	// print
-	stack<int> result;
-	temp = min(a.first[N - 1] + x.first, a.second[N - 1] + x.second);
-	printf("%d\n", temp);
-	bool rec = (temp == a.second[N - 1] + x.second);
-	result.push(rec ? 2 : 1);
+    int result1[count], result2[count];
+    result1[0] = e1 + a1[0];
+    result2[0] = e2 + a2[0];
 
-	for (int i = r.first.size() - 1; i + 1; --i)
-	{
-		if (!rec) {
-			result.push(r.first[i] ? 2 : 1);
-			rec = (r.first[i]);
-		}
-		else {
-			result.push(r.second[i] ? 2 : 1);
-			rec = (r.second[i]);
-		}
-	}
+    result[0] = (result1[0] <= result2[0]) + 1;
 
-	for (int i = 0; i <= (int)r.first.size(); ++i)
-	{
-		printf("%d %d\n", result.top(), i + 1);
-		result.pop();
-	}
+    for(int i = 1; i < count; ++i) 
+    {
+        result1[i] = min(result1[i-1] + a1[i], result2[i-1] + t2[i-1] + a1[i]);
+        result2[i] = min(result2[i-1] + a2[i], result1[i-1] + t1[i-1] + a2[i]);
 
-	return 0;
+        r1[i] = ((result1[i-1] + a1[i] > result2[i-1] + t2[i-1] + a1[i]) + 1);
+        r2[i] = ((result2[i-1] + a2[i] <= result1[i-1] + t1[i-1] + a2[i]) + 1);
+    }
+
+    int res1 = result1[count-1] + ex1, res2 = result2[count-1] + ex2;
+
+    // Set last edge to find reverse order
+    if (res1 <= res2) 
+    {
+        route = r1;
+        result[count-1] = 1;
+    } else 
+    {
+        route = r2;
+        result[count-1] = 2;
+    }
+
+    printf("%d\n", min(res1, res2));
+
+    for (int i = count-1; i > 0; --i) 
+    {
+        result[i-1] = route[i];
+        if (route[i] == 2) {
+            route = r2;
+        } else if (route[i] == 1) {
+            route = r1;
+        }
+    }
+    for(int i=0; i < count; ++i) 
+        printf("%d %d \n", result[i], i+1);
+
+    free(a1);
+    free(a2);
+    free(t1);
+    free(t2);
+    free(r1);
+    free(r2);
+    free(result);
+
+    return 0;
 }
-
-/*
-6
-2 4
-3 2
-7 9 3 4 8 4
-8 5 6 4 5 7
-2 3 1 3 4
-2 1 2 2 1
-*/
